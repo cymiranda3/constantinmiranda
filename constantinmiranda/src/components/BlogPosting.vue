0a4881b41da786ca63fa63048b596ae2437b364d
Constantin Miranda <cym8@cornell.edu>
2019-04-18 17:57:09 -0400

    inital commit with some work done on blog posts.

---
+++ constantinmiranda/src/components/BlogPosting.vue
@@ -0,0 +1,55 @@
+<template>
+    <b-card no-body class="overflow-hidden singlePost">
+      <b-row no-gutters>
+        <b-col md="6">
+          <b-card-img src="https://picsum.photos/400/400/?image=20" class="rounded-0"></b-card-img>
+        </b-col>
+        <b-col md="6">
+          <b-card-body :title='blogTitle'>
+            <b-card-text>
+              {{ blogSubtitle }}
+            </b-card-text>
+          </b-card-body>
+        </b-col>
+      </b-row>
+      <b-button class="moduleLinkButton" href="#" variant="primary">Go somewhere</b-button>
+    </b-card>
+</template>
+
+<script>
+export default {
+  name: "BlogPosting",
+  props: {
+    blogImage: String,
+    blogTitle: String,
+    blogSubtitle: String,
+    blogDate: String,
+    blogBody: String,
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+/* Styling for blog post card from here on: */
+.singlePost {
+  max-width: 700px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.moduleLinkButton {
+  border-radius: 0px 0px 5px 5px;
+}
+</style>
---
+++ constantinmiranda/src/views/Blog.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="blog">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <BlogPost blogTitle="Why Vue is the superior framework." blogSubtitle="A few musings from a Cornell Student." blogDate="January 21st, 2019" blogBody="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import BlogPost from "@/components/BlogPosting.vue";
+
+export default {
+  name: "blog",
+  components: {
+    BlogPost
+  }
+};
+</script>
