cd6530909e0a4662821b631e45f37a16d5ee521f
Constantin Miranda <cym8@cornell.edu>
2019-04-23 14:42:01 -0400

    buttons working for scrolling

--- constantinmiranda/src/components/Biography.vue
+++ constantinmiranda/src/components/Biography.vue
@@ -1,29 +1,44 @@
 <template>
   <div class="bio">
-    <b-card img-src='card' img-alt="Card image" img-left class="mb-3">
-      <b-card-text>
-        Some quick example text to build on the card and make up the bulk of the card's content.
-      </b-card-text>
-    </b-card>
+    <b-card id="bioCard" no-body class="overflow-hidden">
+    <b-row no-gutters>
+      <b-col md="6">
+        <b-card-img id="bioCardImage" :src="require('../assets/images/snowbird.jpg')" class="rounded-0"></b-card-img>
+      </b-col>
+      <b-col md="6">
+        <b-card-body title="Currently @ Strategic Investment Group">
+          <b-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </b-card-text>
+        </b-card-body>
+      </b-col>
+    </b-row>
+  </b-card>
   </div>
 </template>
 
 <script>
-import foot1 from '../assets/images/snowbird.jpg';
 export default {
   name: "Biography",
-  data() {
-    card: foot1;
-  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .bio {
-    width: 80vw;
     margin-left: auto;
     margin-right: auto;
+  }
+
+  #bioCard {
+    width: 70vw;
+    min-width: 150px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  #bioCardImage {
+    min-height: 261px;
   }
 
 </style>
--- constantinmiranda/src/views/Home.vue
+++ constantinmiranda/src/views/Home.vue
@@ -1,7 +1,38 @@
 <template>
   <div class="home">
-    <Biography />
-    <h1 class='sectionHeader'>Education</h1>
+    <section class="company-heading intro-type" id="parallax-one">
+      <div class="container">
+        <div class="row product-title-info">
+          <div class="col-md-12">
+            <Biography />
+            <a class="ct-btn-scroll ct-js-btn-scroll" href="#education"><img alt="Arrow Down Icon" src="https://www.solodev.com/assets/anchor/arrow-down.png"></a>
+          </div>
+        </div>
+      </div>
+      <div class="parallax" id="parallax-cta"></div>
+    </section>
+
+    <div class="main">
+      <section id="education">
+        <div class="container">
+          <h1 class='sectionHeader'>Education</h1>
+          <a class="ct-btn-scroll ct-js-btn-scroll" href="#skills"><img alt="Arrow Down Icon" src="https://www.solodev.com/assets/anchor/arrow-down.png"></a>
+        </div>
+      </section>
+
+      <section id="skills">
+        <div class="container">
+          <h1 class='sectionHeader'>Skills</h1>
+        </div>
+      </section>
+
+      <section id="work-experience">
+        <div class="container">
+          <h1 class='sectionHeader'>Work Experience</h1>
+        </div>
+      </section>
+    </div>
+
   </div>
 </template>
 
@@ -9,11 +40,25 @@
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
 import Biography from "@/components/Biography.vue";
+import $ from 'jquery'
 
 export default {
   name: "home",
   components: {
     Biography,
+  },
+  mounted: function() {
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
   }
 };
 </script>
@@ -25,5 +70,54 @@ export default {
     color: black;
     text-align: left;
     margin-left: 40px;
+  }
+
+  .sectionWrapper {
+    margin-top: 3vw;
+    margin-bottom: 18vh;
+  }
+
+  /* For scrolling */
+  .intro-type .container {
+    padding: 107px 0px 102px 0px !important;
+  }
+
+
+  .parallax {
+    background-attachment: inherit !important;
+    background-repeat: repeat;
+    background-size: cover;
+    position: absolute;
+    top: 0px;
+    bottom: 0px;
+    width: 100%;
+    z-index: -10;
+  }
+
+  .ct-btn-scroll {
+    color: black;
+    width: 49px;
+    height: 49px;
+    position: relative;
+    bottom: 30px;
+    left: 0;
+    right: 0;
+    top: -20px;
+    margin: auto;
+    padding-top: 500px;
+    z-index: 9;
+    border-radius: 50%;
+    -webkit-transition: background-color 0.25s ease-in-out;
+    transition: background-color 0.25s ease-in-out;
+  }
+
+  img,
+  figure {
+    max-width: 100%;
+  }
+
+  #education, #skills, #work-experience {
+    height: 100vh;
+    padding-top: 30px;
   }
 </style>
